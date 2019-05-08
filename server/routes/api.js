8b6f48c8ddc24478c301495f600e002fe257ee0c
zaki Abdullah <zabdullah94@gmail.com>
2019-05-08 13:34:40 -0400

    Create index file

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,20 +7,26 @@ router.get('/ping', (req, res) => res.json(true));
 
 
 
-const data = [];
-  for( let i =0; i < 4; i ++){
-      data.push(
-        {
-            key: i,
-            experiment: 'Clinical Testing ' + i,
-            project: "Methylscape",
-            investigator: 'Zeid Abdullaev ' + i,
-            date: '12/03/2018'
-          }
-      )
-}
+// const data = [];
+//   for( let i =0; i < 40; i ++){
+//       data.push(
+//         {
+//             key: i,
+//             experiment: 'Clinical Testing ' + i,
+//             project: "Methylscape",
+//             investigator: 'Zeid Abdullaev ' + i,
+//             date: '12/03/2018'
+//           }
+//       )
+// }
 
 router.get('/test', (req, res) => res.json(data))
+
+const test = require('../scripts/createIndexFile').Request
+
+router.get('/methylScapeIndexFile', (req, res) =>{
+  res.json(test.getIndexFile)
+})
 
 
 module.exports = router;
--- server/scripts/createIndexFile.js
+++ server/scripts/createIndexFile.js
@@ -1,67 +1,95 @@
 const AWS = require('aws-sdk');
 var s3tree = require('s3-tree');
 
-getS3tree = async() => {
-  var generator = s3tree({bucket: 'cbiit-tools-data'});
-  const s3Tree = await generator.generate('/methylscape/');
-  const tree = await createMethylTree(s3Tree);
-  console.log(JSON.stringify(tree, null, 4))
+getTree = async () => {
+    const s3Tree = await s3tree({bucket: 'cbiit-tools-data'}).generate('/methylscape/');
+    const tree = await createMethylTree(s3Tree);
+    return tree
 }
 
-createMethylTree = async(s3Tree) => {
-  var methylTree = {}
-  for(folder in s3Tree){
-    methylTree[folder] = {}
-    methylTree[folder]["files"] = {}
-    for(file in s3Tree[folder]){
-      if(file === "Metadata.csv"){
-        methylTree[folder]["metaData"] = await parseMetaFile(s3Tree[folder][file], folder)
-      }else{
-        methylTree[folder]["files"][file] = s3Tree[folder][file]
+createMethylTree = async (s3Tree) => {
+    var methylTree = {}
+    for (folder in s3Tree) {
+      if(folder === "indexFile.json"){
+        continue;
       }
+        methylTree[folder] = {}
+        methylTree[folder]["files"] = {}
+        for (file in s3Tree[folder]) {
+            if (file === "Metadata.csv") {
+                methylTree[folder]["metaData"] = await parseMetaFile(s3Tree[folder][file], folder)
+            } else {
+                methylTree[folder]["files"][file] = s3Tree[folder][file]
+            }
+        }
     }
-  }
-  return methylTree
+    return methylTree
 }
 
-parseMetaFile = async(metaFileKey, folder) => {
-  var metaData = await getMetaData(metaFileKey).then(res => {
-    var data = {}
-    data["header"] = res[7]
-    data["data"] = []
-    var i = 0;
-    for(i = 8; i < res.length; i++){
-      data["data"] = [...data["data"], res[i]]
-    }
-    return {
-      "investigator"    : res[1][1],
-      "project"         : res[2][1],
-      "experiment"      : res[3][1],
-      "date"            : res[4][1], 
-      "data"            : data
-    }
-  })
-  return metaData
-  
-}
-
-getMetaData = (metaFileKey) => new Promise( resolve => {
-  var params = {
-    Bucket    : 'cbiit-tools-data',
-    Key       : metaFileKey
-  };
-  var s3 = new AWS.S3();
-  const s3Stream = s3.getObject(params).createReadStream()
-  var metaFileArray = []
-  require('fast-csv').fromStream(s3Stream)
-    .on('data', (data) => {
-      metaFileArray = [...metaFileArray, data]
+parseMetaFile = async (metaFileKey, folder) => {
+    var metaData = await getMetaData(metaFileKey).then(res => {
+        var data = {}
+        data["header"] = res[7]
+        data["data"] = []
+        var i = 0;
+        for (i = 8; i < res.length; i++) {
+            data["data"] = [...data["data"], res[i]]
+        }
+        return {
+            "investigator": res[1][1],
+            "project": res[2][1],
+            "experiment": res[3][1],
+            "date": res[4][1],
+            "data": data
+        }
     })
-    .on('end', () => {
-      resolve(metaFileArray)
-    }
-  )
+    return metaData
+
+}
+
+getMetaData = (metaFileKey) => new Promise(resolve => {
+    var params = {
+        Bucket: 'cbiit-tools-data',
+        Key: metaFileKey
+    };
+    var s3 = new AWS.S3();
+    const s3Stream = s3.getObject(params).createReadStream()
+    var metaFileArray = []
+    require('fast-csv').fromStream(s3Stream)
+        .on('data', (data) => {
+            metaFileArray = [...metaFileArray, data]
+        })
+        .on('end', () => {
+            resolve(metaFileArray)
+        })
 })
 
+uploadDataAsJSON = async(data) => {
+  return new Promise(function(resolve, reject) {
+  var s3 = new AWS.S3()
+  var params = {
+    Bucket: 'cbiit-tools-data/methylscape',
+    Key: 'indexFile.json',
+    Body: JSON.stringify(data),
+    ContentType: "application/json"
+  }
+  s3.putObject(params , (err,data) => {
+      if(err) reject(err)
+      else resolve(data)
+    }
+  );
+  })
+}
 
-getS3tree()
+createIndexFile = async () => {
+  const data = await getTree()
+  uploadDataAsJSON(data).then(res => console.log(res))
+  return data 
+}
+
+createIndexFile.prototype.getIndexFile = async () => {
+    const data = await createIndexFile()
+    return data
+}
+
+exports.Request = createIndexFile
