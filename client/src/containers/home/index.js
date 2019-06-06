05a3b8cf80b0a690951927249bd0f0d839bcff32
zaki Abdullah <zabdullah94@gmail.com>
2019-06-06 15:21:39 -0400

    UI layout update:

--- client/src/containers/home/index.js
+++ client/src/containers/home/index.js
@@ -1,13 +1,42 @@
 import React from 'react'
-import ResultsTable from './components/resultsTable'
+import { Tabs, PageHeader } from "antd";
+import Summary from "./components/Summary";
+import Experiments from "./components/Experiments";
+import Samples from "./components/Samples";
+import Projects from "./components/Projects";
+
+
 
 import { connect } from 'react-redux'
 
-const Home = props => (
-  <div>
-    <ResultsTable/>
-  </div>
-)
+const TabPane = Tabs.TabPane;
+
+class Home extends React.Component {
+
+  state = {
+    projectID: "",
+  };
+
+  render() {
+    return (
+      <div>
+        <PageHeader />
+        <Tabs defaultActiveKey="project" style={{ theme: 'dark'}}>
+          <TabPane tab="Project" key="project">
+            <Projects projectID={this.state.projectID}/>
+          </TabPane>
+          <TabPane tab="Experiments" key="experiments">
+            <Experiments projectID={this.state.projectID}/>
+          </TabPane>
+          <TabPane tab="Samples" key="samples">
+            <Samples projectID={this.state.projectID}/>
+          </TabPane>
+        </Tabs>
+      </div>
+    )
+  }
+
+}
 
 export default connect(
 )(Home)
