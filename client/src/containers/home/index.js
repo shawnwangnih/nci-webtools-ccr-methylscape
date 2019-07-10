1bd573d99dfc18406404d28c380fa4848fd7ed83
zaki Abdullah <zabdullah94@gmail.com>
2019-07-10 11:09:47 -0400

    Update API call

--- client/src/containers/home/index.js
+++ client/src/containers/home/index.js
@@ -1,14 +1,12 @@
-import React from 'react'
-import { Route , Link} from "react-router-dom";
+import React from "react";
+import { Route, Link } from "react-router-dom";
 import { Tabs, PageHeader } from "antd";
 // import Summary from "./components/Summary";
 import Experiments from "./components/Experiments";
 import Samples from "./components/Samples";
 import Projects from "./components/Projects";
-import AWS from 'aws-sdk'
 
-
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
 
 const TabPane = Tabs.TabPane;
 
@@ -16,66 +14,51 @@ class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [],
-      // key: "project"
-    }
-    this.handler = this.handler.bind(this)
-  };
+      data: []
+    };
+    this.handler = this.handler.bind(this);
+  }
 
   async handler(someValue) {
     this.setState({
       key: someValue
-    })
+    });
   }
 
   async componentDidMount() {
-    await this.scanTable()
-  }
-
-  async callback(key) {
-    console.log(key);
-  }
-
-
-  async scanTable() {
-
-    var documentClient = new AWS.DynamoDB.DocumentClient(
-      {
-        apiVersion: '2012-08-10',
-        "region": 'us-east-1',
-
+    fetch("http://localhost:5000/api/methylScapeTableData", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        this.setState({ data });
+        this.setState({ loading: false });
       });
-    const params = { TableName: "MethylscapeSamples-prod" }
-    let scanResults = [];
-    let items;
-    do{
-        items =  await documentClient.scan(params).promise();
-        items.Items.forEach((item) => scanResults.push(item));
-        params.ExclusiveStartKey  = items.LastEvaluatedKey;
-    }while(typeof items.LastEvaluatedKey != "undefined");
-    this.setState({data:scanResults})
-};
+  }
 
   render() {
     return (
       <div>
         {/* <PageHeader /> */}
-        <Tabs >
-          <TabPane tab="Project" key="project" >
+        <Tabs>
+          <TabPane tab="Project" key="project">
             <Projects data={this.state.data} action={this.handler} />
           </TabPane>
           <TabPane tab="Experiments" key="experiments">
-            <Experiments data={this.state.data}/>
+            <Experiments data={this.state.data} />
           </TabPane>
           <TabPane tab="Samples" key="samples">
-            <Samples data={this.state.data}/>
+            <Samples data={this.state.data} />
           </TabPane>
         </Tabs>
       </div>
-    )
+    );
   }
-
 }
 
-export default connect(
-)(Home)
+export default connect()(Home);
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,41 +1,21 @@
 const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser')
+const config = require('../../config.json');
 const router = express.Router();
-const AWS = require('aws-sdk');
-AWS.config.update({region: 'us-east-1'});
+const scanDynamoDB = require('./scanDynamoDB')
+
 
 router.use(cors());
 router.use(bodyParser.json())
 
 router.get('/ping', (req, res) => res.json(true));
 
-router.get('/methylScapeIndexData', (req, res) =>{
-  var documentClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
-  const params = { TableName: "MethylscapeSamples-prod" }
-  let scanResults = [];
-  documentClient.scan(params, onScan)
-  function onScan(err, data){
-    if (err) {
-        console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
-    } else {
-        console.log("Scan succeeded.");
-        data.Items.forEach(function(itemdata) {
-          console.log("Item :", JSON.stringify(itemdata));
-          scanResults.push(itemdata)
-        });
-        // data.Items.forEach((itemdata) => scanResults.push(itemdata));
-        // continue scanning if we have more items
-        if (typeof data.LastEvaluatedKey != "undefined") {
-            console.log("Scanning for more...");
-            params.ExclusiveStartKey = data.LastEvaluatedKey;
-            docClient.scan(params, onScan);
-        }
-    }
+router.get('/methylScapeTableData', (req, res) => {
+  scanDynamoDB.scanTable(config.methylscapeTable).then(data => res.json(data))
+
   }
-  console.log("Scanning for more...");
-  // res.json(scanResults)
-})
+)
 
 // router.get('/methylScapeIndexFile', (req, res) =>{
 //   createIndexFile.getIndexFile().then(data => res.json(data))
