43a4ea9b9db5d5710c5fdec3d59fda2e5c82e1b9
zaki Abdullah <zabdullah94@gmail.com>
2019-07-22 11:05:03 -0400

    remoce server

---
+++ src/containers/app/components/navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Menu } from 'antd'
+
+class Navbar extends React.Component {
+    state = {
+        current: 'home',
+    }
+    handleClick = (e) => {
+        console.log('click ', e);
+        this.setState({
+          current: e.key,
+        });
+    }
+    render() {
+        return (
+            <Menu
+              onClick={this.handleClick}
+              selectedKeys={[this.state.current]}
+              mode="horizontal"
+              theme="light"
+            >
+              <Menu.Item key="home">
+                  <Link to="/">Home</Link>
+              </Menu.Item>
+
+              <Menu.Item key="cns">
+                  <Link to="/cns-profiling">CNS Profiling</Link>
+              </Menu.Item>
+
+              <Menu.Item key="help">
+                  <Link to="/help">Help</Link>
+              </Menu.Item>
+            </Menu>
+        )
+    }
+
+}
+
+
+export default Navbar
---
+++ src/containers/home/utils/scanDynamoDB.js
@@ -0,0 +1,20 @@
+// const AWS = require('aws-sdk');
+import AWS  from 'aws-sdk'
+
+export let scanTable = async(tableName) =>{
+    AWS.config.update({region: 'us-east-1'});
+    console.log("TEST")
+    var documentClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+    const params = {
+        TableName: tableName,
+    };
+    let scanResults = [];
+    let items;
+    do{
+        items =  await documentClient.scan(params).promise();
+        items.Items.forEach((item) => scanResults.push(item));
+        params.ExclusiveStartKey  = items.LastEvaluatedKey;
+    }while(typeof items.LastEvaluatedKey != "undefined");
+
+    return scanResults;
+};
