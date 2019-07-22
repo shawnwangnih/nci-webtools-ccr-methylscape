29c140e87798deccbe693f0e906ab5b0f6357eb2
zaki Abdullah <zabdullah94@gmail.com>
2019-07-22 11:25:17 -0400

    fix untracked files

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { ConnectedRouter } from 'connected-react-router'
 import store, { history } from './store'
 import App from './containers/app'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import 'sanitize.css/sanitize.css'
 import './index.css'
 
