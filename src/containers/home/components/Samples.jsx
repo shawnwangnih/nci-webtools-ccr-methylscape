54ba1cb8818189c3beb72a98f048418c9a3355d0
zaki Abdullah <zabdullah94@gmail.com>
2019-07-23 22:21:06 -0400

    ass search to columns to samples

--- src/containers/home/components/Samples.jsx
+++ src/containers/home/components/Samples.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Table, Input, Button, Form, Select } from "antd";
+import Highlighter from 'react-highlight-words';
+import { Table, Input, Button, Form, Select, Icon } from "antd";
 
 class Samples extends React.Component {
   constructor(props) {
@@ -20,6 +21,66 @@ class Samples extends React.Component {
       filteredData: []
     };
   }
+
+  getColumnSearchProps = dataIndex => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={node => {
+            this.searchInput = node;
+          }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => this.handleSearch(selectedKeys, confirm)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Button
+          type="primary"
+          onClick={() => this.handleSearch(selectedKeys, confirm)}
+          icon="search"
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          Search
+        </Button>
+        <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+          Reset
+        </Button>
+      </div>
+    ),
+    filterIcon: filtered => (
+      <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: visible => {
+      if (visible) {
+        setTimeout(() => this.searchInput.select());
+      }
+    },
+    render: text => (
+      <Highlighter
+        highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+        searchWords={[this.state.searchText]}
+        autoEscape
+        textToHighlight={text.toString()}
+      />
+    ),
+  });
+
+  handleSearch = (selectedKeys, confirm) => {
+    confirm();
+    this.setState({ searchText: selectedKeys[0] });
+  };
+
+  handleReset = clearFilters => {
+    clearFilters();
+    this.setState({ searchText: '' });
+  };
 
   async componentWillReceiveProps(nextProps) {
     if(nextProps.filter.project){
@@ -64,7 +125,6 @@ class Samples extends React.Component {
   }
 
 
-
   getMF = data => {
     return (Object.keys(data).length > 2) ? Object.keys(data["0"]) : ""
   }
@@ -103,71 +163,85 @@ class Samples extends React.Component {
         sorter: true,
         width: "300",
         fixed: 'left',
+        ...this.getColumnSearchProps('sample_name'),
       },{
         title: "Project",
         dataIndex: "project",
         sorter: true,
-        width: "300"
+        width: "300",
+        ...this.getColumnSearchProps('project'),
       },{
         title: "Experiment",
         dataIndex: "experiment",
         sorter: true,
         width: "300",
+        ...this.getColumnSearchProps('experiment'),
         render: (text, record) => <a onClick={() => this.props.changeTab("experiments", {project:record.project})}>{text}</a>
       },{
         title: "Date",
         dataIndex: "date",
         sorter: true,
+        ...this.getColumnSearchProps('date'),
         width: "200"
       },{
         title: "Surgical Case",
         dataIndex: "surgical_case",
         sorter: true,
+        ...this.getColumnSearchProps('surgical_case'),
         width: "200"
       },{
         title: "Gender",
         dataIndex: "gender",
         sorter: true,
+        ...this.getColumnSearchProps('gender'),
         width: "200"
       },{
         title: "Age",
         dataIndex: "age",
         sorter: true,
+        ...this.getColumnSearchProps('age'),
         width: "200"
       },{
         title: "Diagnosis",
         dataIndex: "diagnosis",
         sorter: true,
+        ...this.getColumnSearchProps('diagnosis'),
         width: "200"
       },{
         title: "Methylation Family (MF)",
         dataIndex: "family",
         sorter: true,
+        ...this.getColumnSearchProps('family'),
         width: "200",
       },{
         title: "MF Calibrated Scores",
         dataIndex: "family_score",
         sorter: true,
+        ...this.getColumnSearchProps('family_score'),
         width: "200"
       },{
         title: "Methylation Class (MC)",
         dataIndex: "class",
         sorter: true,
+        ...this.getColumnSearchProps('class'),
         width: "200"
       },{
         title: "MC Calibrated Scores",
         dataIndex: "class_score",
         sorter: true,
+        ...this.getColumnSearchProps('class_score'),
         width: "200"
       },{
         title: "MGMT status",
         dataIndex: "mgmt_prediction.Status",
         sorter: true,
+        ...this.getColumnSearchProps('mgmt_prediction.Status'),
         width: "200"
       },{
         title: "MGMT score",
         dataIndex: "mgmt_prediction.Estimated",
         sorter: true,
+        ...this.getColumnSearchProps('mgmt_prediction.Estimated'),
         width: "200"
       },{
         title: "t-SNE plot",
@@ -191,6 +265,7 @@ class Samples extends React.Component {
         title: "Notes",
         dataIndex: "notes",
         sorter: true,
+        ...this.getColumnSearchProps('notes'),
         width: "200"
       }
     ];
@@ -246,6 +321,7 @@ class Samples extends React.Component {
             dataSource={this.state.filteredData}
             onChange={this.handleTableChange}
             scroll={{ x: 3500 }}
+
           />
         </div>
       </div>
