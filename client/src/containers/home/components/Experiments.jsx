c7e2c0589baa303d796194d8704c8ab16cefb333
Brian Shen <27040828+naiyume@users.noreply.github.com>
2019-09-18 14:50:42 -0400

    attempted to make the pdf files open

--- client/src/containers/home/components/Experiments.jsx
+++ client/src/containers/home/components/Experiments.jsx
@@ -37,28 +37,7 @@ class Experiments extends React.Component {
   }
 
   showFile(blob) {
-    // It is necessary to create a new blob object with mime-type explicitly set
-    // otherwise only Chrome works like it should
-    var newBlob = new Blob([blob], { type: 'application/pdf' });
-
-    // IE doesn't allow using a blob object directly as link href
-    // instead it is necessary to use msSaveOrOpenBlob
-    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-      window.navigator.msSaveOrOpenBlob(newBlob);
-      return;
-    }
-
-    // For other browsers:
-    // Create a link pointing to the ObjectURL containing the blob.
-    const data = window.URL.createObjectURL(newBlob);
-    var link = document.createElement('a');
-    link.href = data;
-    link.download = 'file.pdf';
-    link.click();
-    setTimeout(function() {
-      // For Firefox it is necessary to delay revoking the ObjectURL
-      window.URL.revokeObjectURL(data);
-    }, 100);
+   return
   }
 
   downloadFile = (experiment, file) => {
@@ -75,7 +54,14 @@ class Experiments extends React.Component {
       })
     })
       .then(res => res.blob())
-      .then(this.showFile)
+      .then(function(blob) { // (**)
+
+        return URL.createObjectURL(blob);
+      
+      })
+      .then(url => {
+        window.open(url, '_blank');
+      })
       /*
       .then(blob => {
         fileSaver(blob, file);
