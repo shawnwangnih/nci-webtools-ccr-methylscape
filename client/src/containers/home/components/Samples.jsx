c1767ad3c5488eae9b78d05ce24e04c0d2127d95
Shen <shenbn@nci-02134517-ml.nci.nih.gov>
2019-10-21 12:19:23 -0400

    start changing the search bar

--- client/src/containers/home/components/Samples.jsx
+++ client/src/containers/home/components/Samples.jsx
@@ -178,7 +178,9 @@ class Samples extends React.Component {
 
   getMF = data => {
     console.log(JSON.stringify(data));
-    return Object.keys(data).length >= 2 ? String(Object.keys(data['0'])[0]).substring(25) : '';
+    return Object.keys(data).length >= 2
+      ? String(Object.keys(data['0'])[0]).substring(25)
+      : '';
   };
 
   getMFScore = data => {
