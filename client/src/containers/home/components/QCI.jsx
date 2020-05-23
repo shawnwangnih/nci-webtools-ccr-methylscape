e8f4a6dc3d66bc9b1d9674299ffd8c620f6f9c5b
Phyllip Cho <phyllip3@users.noreply.github.com>
2020-05-22 23:36:27 -0400

    manage expanded rows

--- client/src/containers/home/components/QCI.jsx
+++ client/src/containers/home/components/QCI.jsx
@@ -6,40 +6,6 @@ import './QCI.css';
 
 export default function QCI() {
   const { id, file } = useParams();
-  console.log('params', id, file);
-
-  useEffect(() => {
-    downloadFile(id, file);
-  }, []);
-
-  //Helper to download files from the s3 bucket
-  async function downloadFile(sampleId, file) {
-    const root =
-      process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:8290/' : window.location.pathname;
-
-    try {
-      let response = await fetch(`${root}getMethylScapeFile`, {
-        method: 'POST',
-        body: JSON.stringify({
-          sampleId: sampleId,
-          fileName: file,
-        }),
-      });
-      if (response.status == 404) {
-        Modal.error({
-          title: 'Could Not Find QCI Report',
-          content: 'some messages...some messages...',
-        });
-        console.log(response);
-      } else {
-        let xml = await response.text();
-        console.log(xml);
-        parseVariants(xml);
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  }
 
   const [snvTable, setSnv] = useState({
     columns: [
@@ -75,6 +41,8 @@ export default function QCI() {
       { title: 'TIER**', dataIndex: 'tier', align: 'center' },
     ],
     data: [],
+    expandedRowKeys: [],
+    onExpand: (expanded, record) => manageExpandedRows(expanded, record, setSnv),
   });
   const [cnvTable, setCnv] = useState({
     columns: [
@@ -98,6 +66,8 @@ export default function QCI() {
       { title: 'TIER**', dataIndex: 'tier', align: 'center' },
     ],
     data: [],
+    expandedRowKeys: [],
+    onExpand: (expanded, record) => manageExpandedRows(expanded, record, setCnv),
   });
   const [fusionTable, setFusion] = useState({
     columns: [
@@ -122,6 +92,8 @@ export default function QCI() {
       { title: 'TIER**', dataIndex: 'tier', align: 'center' },
     ],
     data: [],
+    expandedRowKeys: [],
+    onExpand: (expanded, record) => manageExpandedRows(expanded, record, setFusion),
   });
   const [unkTable, setUnk] = useState({
     columns: [
@@ -146,6 +118,68 @@ export default function QCI() {
     ],
     data: [],
   });
+
+  useEffect(() => {
+    downloadFile(id, file);
+  }, []);
+
+  // manage keys of expanded rows
+  function manageExpandedRows(expanded, record, setter) {
+    expanded
+      ? setter((prevTable) => ({
+          ...prevTable,
+          ...{ expandedRowKeys: [...prevTable.expandedRowKeys, record.key] },
+        }))
+      : setter((prevTable) => ({
+          ...prevTable,
+          ...{ expandedRowKeys: prevTable.expandedRowKeys.filter((key) => key != record.key) },
+        }));
+  }
+
+  //Helper to download files from the s3 bucket
+  async function downloadFile(sampleId, file) {
+    const root =
+      process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:8290/' : window.location.pathname;
+
+    try {
+      let response = await fetch(`${root}getMethylScapeFile`, {
+        method: 'POST',
+        body: JSON.stringify({
+          sampleId: sampleId,
+          fileName: file,
+        }),
+      });
+      if (response.status == 404) {
+        Modal.error({
+          title: 'Could Not Find QCI Report',
+          content: 'some messages...some messages...',
+        });
+        // console.log(response);
+      } else {
+        let xml = await response.text();
+        // console.log(xml);
+        parseVariants(xml);
+      }
+    } catch (e) {
+      // console.log(e);
+    }
+  }
+
+  function parseUpload(file) {
+    let reader = new FileReader();
+    reader.onload = () => {
+      const result = xml2js(reader.result, { compact: true });
+      let variants = result.report.variant;
+      !Array.isArray(variants) ? (variants = [variants]) : (variants = variants.reverse());
+      const significant = variants.filter((v) => v.assessment._text.match(/pathogenic/gi));
+      const uncertain = variants.filter((v) => v.assessment._text.match(/uncertain/gi));
+      // console.log('sig', significant);
+      // console.log('un', uncertain);
+      sigVariants(significant);
+      unknownVariants(uncertain);
+    };
+    reader.readAsText(file);
+  }
 
   // display gene with reference
   function displayGeneRef(gene) {
@@ -179,8 +213,8 @@ export default function QCI() {
 
   function parseVariants(file) {
     const parsed = xml2js(file, { compact: true });
-    let variants = parsed.report.variant.reverse();
-    if (!Array.isArray(variants)) variants = [variants];
+    let variants = parsed.report.variant;
+    !Array.isArray(variants) ? (variants = [variants]) : (variants = variants.reverse());
     const significant = variants.filter((v) => v.assessment._text.match(/pathogenic/gi));
     const uncertain = variants.filter((v) => v.assessment._text.match(/uncertain/gi));
     // console.log('sig', significant);
@@ -237,9 +271,10 @@ export default function QCI() {
         });
       }
     });
-    setSnv({ ...snvTable, ...{ data: snvData } });
-    setCnv({ ...cnvTable, ...{ data: cnvData } });
-    setFusion({ ...fusionTable, ...{ data: fusionData } });
+
+    setSnv((snvTable) => ({ ...snvTable, ...{ data: snvData } }));
+    setCnv((cnvTable) => ({ ...cnvTable, ...{ data: cnvData } }));
+    setFusion((fusionTable) => ({ ...fusionTable, ...{ data: fusionData } }));
   }
 
   function unknownVariants(variants) {
@@ -263,7 +298,23 @@ export default function QCI() {
     <div>
       <div>
         <div>
-          <button onClick={() => downloadFile(id, file)}>s3</button>
+          <label>Upload Here</label>
+          <input
+            type="file"
+            id="xmlInput"
+            onChange={(e) => {
+              parseUpload(e.target.files[0]);
+            }}
+          />
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              // downloadFile(id, file);
+            }}
+          >
+            s3
+          </button>
         </div>
         <div>
           <div>
@@ -272,7 +323,10 @@ export default function QCI() {
               columns={snvTable.columns}
               dataSource={snvTable.data}
               pagination={false}
+              expandRowByClick={true}
               expandedRowRender={(record) => renderHTML(record.interpretation)}
+              expandedRowKeys={snvTable.expandedRowKeys}
+              onExpand={snvTable.onExpand}
               bordered
               title={() => <h3>SMALL NUCLEOTIDE VARIANTS</h3>}
               footer={() => (
@@ -283,7 +337,10 @@ export default function QCI() {
               columns={cnvTable.columns}
               dataSource={cnvTable.data}
               pagination={false}
+              expandRowByClick={true}
               expandedRowRender={(record) => renderHTML(record.interpretation)}
+              expandedRowKeys={cnvTable.expandedRowKeys}
+              onExpand={cnvTable.onExpand}
               bordered
               title={() => <h3>STRUCTURAL VARIANTS: COPY NUMBER VARIATION (CNV)</h3>}
               footer={() => (
@@ -301,7 +358,10 @@ export default function QCI() {
               columns={fusionTable.columns}
               dataSource={fusionTable.data}
               pagination={false}
+              expandRowByClick={true}
               expandedRowRender={(record) => renderHTML(record.interpretation)}
+              expandedRowKeys={fusionTable.expandedRowKeys}
+              onExpand={fusionTable.onExpand}
               bordered
               title={() => <h3>STRUCTURAL VARIANTS: FUSION</h3>}
               footer={() => <sub>**TIER: Actionability Classification</sub>}
@@ -313,9 +373,6 @@ export default function QCI() {
               columns={unkTable.columns}
               dataSource={unkTable.data}
               pagination={false}
-              expandRowByClick={true}
-              size="small"
-              ellipsis={true}
               bordered
               title={() => <h3>STRUCTURAL VARIANTS: COPY NUMBER VARIATION (CNV)</h3>}
               footer={() => <sub>*VAF: Variant Allele Frequency</sub>}
