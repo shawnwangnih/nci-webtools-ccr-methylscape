14d8c9e1fe774082007d99c5286c442843d2ed92
Phyllip Cho <phyllip3@users.noreply.github.com>
2022-01-03 18:07:12 -0500

    overview wip

--- client/src/modules/umap/overview/info.js
+++ client/src/modules/umap/overview/info.js
@@ -1,3 +1,8 @@
+import { PieChart, Building, CloudArrowDown } from 'react-bootstrap-icons';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Plot from 'react-plotly.js';
 import { useRecoilValue } from 'recoil';
 import { overviewState } from './overview.state';
 
@@ -6,7 +11,38 @@ export default function Info() {
 
   return (
     <div>
-      samples: {samples} studies: {studies} institutions: {institutions}
+      <Row>
+        <Col sm="4">
+          <Card bg="light" className="d-flex flex-row">
+            <div className="bg-primary p-2">
+              <PieChart color="white" size="3rem" />
+            </div>
+            <div className="w-100 my-auto text-center">
+              <div className="fw-light">TOTAL SAMPLES</div>
+              <b>{samples}</b>
+            </div>
+          </Card>
+          <Card bg="light" className="d-flex flex-row">
+            <div className="p-2" style={{ background: 'rebeccapurple' }}>
+              <CloudArrowDown color="white" size="3rem" />
+            </div>
+            <div className="w-100 my-auto text-center">
+              <div className="fw-light">NUMBER OF STUDIES</div>
+              <b>{samples}</b>
+            </div>
+          </Card>
+          <Card bg="light" className="d-flex flex-row">
+            <div className="bg-dark p-2">
+              <Building color="white" size="3rem" />
+            </div>
+            <div className="w-100 my-auto text-center">
+              <div className="fw-light">NUMBER OF INSTITUTIONS</div>
+              <b>{samples}</b>
+            </div>
+          </Card>
+        </Col>
+        <Col sm="8"></Col>
+      </Row>
     </div>
   );
 }
