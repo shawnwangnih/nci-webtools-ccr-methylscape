da92b43625a6515baf97210e165263ddc9efec73
Brian Park <park-brian@users.noreply.github.com>
2022-02-08 23:24:19 -0500

    use postgres connection

--- server/server.js
+++ server/server.js
@@ -1,36 +1,31 @@
-const express = require("express");
-const helmet = require("helmet");
-const knex = require('knex')
-const getLogger = require("./services/logger");
-const { apiRouter } = require("./services/api");
-const { forkCluster } = require("./services/cluster");
-const config = require("./config.json");
-const { logErrors } = require("./services/middleware");
-const UserManager = require("./services/auth/userManager");
-const RoleManager = require("./services/auth/roleManager");
-const args = require("minimist")(process.argv.slice(2));
-const isProduction = process.env.NODE_ENV === "production" || args.production;
+const express = require('express');
+const knex = require('knex');
+const getLogger = require('./services/logger');
+const { apiRouter } = require('./services/api');
+const { forkCluster } = require('./services/cluster');
+const config = require('./config.json');
+const { logErrors } = require('./services/middleware');
+const UserManager = require('./services/auth/userManager');
+const RoleManager = require('./services/auth/roleManager');
+const args = require('minimist')(process.argv.slice(2));
+const isProduction = process.env.NODE_ENV === 'production' || args.production;
 
 // fork to multiple processes
 if (forkCluster()) return;
 
 // create app and register locals/middlware
 const app = express();
-const database = knex({
-  client: 'better-sqlite3',
-  connection: {
-    filename: config.database
-  },
-  useNullAsDefault: true
+const connection = knex({
+  client: "pg",
+  connection: config.database
 });
 
-app.locals.logger = getLogger("methylscape-analysis");
-app.locals.database = database;
-app.locals.userManager = new UserManager(database);
-app.locals.roleManager = new RoleManager(database);
+app.locals.logger = getLogger('methylscape-analysis');
+app.locals.connection = connection;
+app.locals.userManager = new UserManager(connection);
+app.locals.roleManager = new RoleManager(connection);
 
-app.use(helmet());
-app.use("/api", apiRouter);
+app.use('/api', apiRouter);
 app.use(logErrors); // logErrors should always be last
 
 // serve static assets during local development
@@ -38,7 +33,7 @@ if (!isProduction) app.use(express.static(config.server.client));
 
 app.listen(config.server.port, () => {
   app.locals.logger.info(
-    `Application is running on port: ${config.server.port}`,
+    `Application is running on port: ${config.server.port}`
   );
 });
 
