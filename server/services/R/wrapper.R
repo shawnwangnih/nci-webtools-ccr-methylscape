5c457bb56b888d9ad3ec561143ab5558b42ecca7
Brian Park <park-brian@users.noreply.github.com>
2022-02-17 22:19:21 -0500

    clean up survival code

--- server/services/R/wrapper.R
+++ server/services/R/wrapper.R
@@ -21,31 +21,30 @@ wrapper <- function(fn, args, paths) {
 }
 
 getSurvivalData <- function(data) {
-    strata <- unique(data$group) 
-   
     survivalFormula <- survival::Surv(overallSurvivalMonths, overallSurvivalStatus) ~ group
     survivalCurves <- survminer::surv_fit(survivalFormula, data = data)
     survivalDataTable <- survminer::surv_summary(survivalCurves, data)
 
-    if (is.null(survivalDataTable$strata)) {
-      survivalDataTable$strata <- strata
-    }
-    
+    # create survival summary table for n.risk at each time point
     survivalSummaryTimes <- survminer:::.get_default_breaks(survivalCurves$time)
     survivalSummary <- summary(survivalCurves, times = survivalSummaryTimes, extend = T)
-    survivalSummaryTable <- data.frame(
-        time = survivalSummary$time,
-        n.risk = survivalSummary$n.risk
+    survivalSummaryTable <- tibble::tibble(
+      time = survivalSummary$time,
+      n.risk = survivalSummary$n.risk,
+      strata = survivalSummary$strata
     )
 
-    if (is.null(survivalSummary$strata)) {
-        survivalSummaryTable$strata <- strata
-    } else {
-        survivalSummaryTable$strata <- survivalSummary$strata
+    # widen summary table across all strata
+    if (!is.null(survivalSummaryTable$strata)) {
+      survivalSummaryTable <- tidyr::pivot_wider(
+        survivalSummaryTable, 
+        names_from = "strata", 
+        values_from="n.risk"
+      )
     }
-    
+
     pValue <- survminer::surv_pvalue(survivalCurves)
-    
+
     list(
         data = survivalDataTable, 
         summary = survivalSummaryTable, 
