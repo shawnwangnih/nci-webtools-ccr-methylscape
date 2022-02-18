4a294b44c37bdea054542a65eab37eacf3a99251
Brian Park <park-brian@users.noreply.github.com>
2022-02-17 22:21:23 -0500

    use survival summary columns

--- client/src/modules/analysis/table/survival-plot.js
+++ client/src/modules/analysis/table/survival-plot.js
@@ -2,28 +2,22 @@ import { useRecoilValue } from 'recoil';
 import Plot from 'react-plotly.js';
 import ReactTable from '../../components/table';
 import { survivalDataSelector } from './table.state';
-import { getSurvivalPlot } from './survival-plot.utils';
+import { getSurvivalPlot, getSummaryColumns } from './survival-plot.utils';
 
 export default function SurvivalPlot() {
   const survivalData = useRecoilValue(survivalDataSelector);
   const survivalPlot = getSurvivalPlot(survivalData?.data);
-  const summaryTableColumns = [
-    'time', 
-    'n.risk', 
-    'strata'
-  ].map((e) => ({ 
-    id: e, 
-    accessor: row => row[e],
-    Header: e 
-  }));
-
+  const summaryTableColumns = getSummaryColumns(survivalData?.summary);
+  
   return (
     <>
       <Plot {...survivalPlot} />
-      {survivalData?.pValue && <div>
-        <strong>p value: </strong>
+      {survivalData?.pValue?.pval && <div>
+        <strong>p-value: </strong>
         {survivalData.pValue[0].pval}
       </div>}
+
+      <h3 className='h5'>Number at Risk</h3>
       <ReactTable
         data={survivalData?.summary || []}
         columns={summaryTableColumns}
