52999a09a49c27f05b6e539802ec7543a2e8984f
Brian Park <park-brian@users.noreply.github.com>
2022-03-02 18:45:05 -0500

    make import faster

--- database/utils.js
+++ database/utils.js
@@ -1,5 +1,7 @@
-import { createReadStream } from "fs";
+import { createReadStream, readFileSync } from "fs";
+import mapValues from "lodash/mapValues.js";
 import { parse } from "csv-parse";
+import * as XLSX from 'xlsx';
 import knex from "knex";
 
 export function createConnection(args) {
@@ -15,12 +17,54 @@ export function createConnection(args) {
     });
 }
 
-export function createCsvRecordIterator(filename, columns) {
+export function createRecordIterator(filename, columns) {
+    const fileExtension = filename.split(".").pop().toLowerCase();
+
+    switch(fileExtension) {
+        case "csv":
+            return createCsvRecordIterator(filename, columns);
+        case "txt":
+            return createCsvRecordIterator(filename, columns, {delimiter: "\t"});
+        case "xlsx":
+            return createExcelRecordIterator(filename, columns);
+        default:
+            throw new Error(`Unsupported file extension: ${fileExtension}`);
+    }
+}
+
+export function createExcelRecordIterator(filename, columns) {
+    const buffer = readFileSync(filename);
+    const workbook = XLSX.read(buffer);
+    const sheet = workbook.Sheets[workbook.SheetNames[0]];
+
+    const [headers] = XLSX.utils.sheet_to_json(sheet, {header: 1, range: 'A1:ZZZ1'});
+    const errors = [];
+
+    for (const { sourceName } of columns) {
+        if (sourceName && !headers.includes(sourceName)) {
+            errors.push(`Missing column: ${sourceName}`)
+        }
+    }
+
+    if (errors.length > 0) {
+        throw new Error(errors.join("\n"));
+    }
+
+    const records = XLSX.utils.sheet_to_json(sheet);
+    const recordParser = createRecordParser(columns);
+
+    return records.map(record => recordParser(
+        mapValues(record, castValue)
+    ));
+}
+
+export function createCsvRecordIterator(filename, columns, options = {}) {
     const parseOptions = {
         columns: true,
         skip_empty_lines: true,
         cast: castValue,
         on_record: createRecordParser(columns),
+        ...options
     };
 
     return createReadStream(filename)
@@ -28,20 +72,30 @@ export function createCsvRecordIterator(filename, columns) {
 }
 
 export async function importTable(connection, iterable, tableName) {
-    let index = 0;
+    let count = 0;
+    let bufferSize = 10000;
+    let buffer = [];
+
+    async function flushBuffer() {
+        await connection.batchInsert(tableName, buffer);
+        count += buffer.length;
+        buffer = [];
+        console.log(`Imported ${count} rows`);
+    }
+
     for await (const record of iterable) {
         try {
-            await connection.insert(record).into(tableName);
+            buffer.push(record);
+            if (buffer.length >= bufferSize)
+                await flushBuffer();
         } catch (error) {
             console.log(record);
             throw error;
         }
-        if (++index % 1000 === 0) {
-            console.log(`Imported ${index} rows`);
-        }
     }
 
-    console.log(`Imported ${index} rows`);
+    await flushBuffer();
+    return count;
 }
 
 export function castValue(value) {
@@ -49,6 +103,8 @@ export function castValue(value) {
         return null;
     } else if (!isNaN(value)) {
         return parseFloat(value);
+    } else if (typeof value === "number" && isNaN(value)) {
+        return null;
     } else {
         return value;
     }
@@ -62,4 +118,12 @@ export function createRecordParser(columns) {
         }
         return row;
     }
+}
+
+export async function withDuration(fn) {
+    const start = Date.now();
+    const results = await fn();
+    const end = Date.now();
+    const duration = (end - start) / 1000;
+    return {results, duration};
 }
