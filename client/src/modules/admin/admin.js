cd9dd750d97610b35ba2db00f50c59727b8de8db
Brian Park <park-brian@users.noreply.github.com>
2022-03-08 18:23:37 -0500

    update admin landing page

--- client/src/modules/admin/admin.js
+++ client/src/modules/admin/admin.js
@@ -1,34 +1,41 @@
-import { Container, Button } from 'react-bootstrap';
-import Table from '../components/table';
+import { Container, Col, Row, Card } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
 export default function Admin() {
-  const users = [
-    { name: 'First Last', email: 'first.last@email.com', roles: 'Admin' },
-    { name: 'First Last', email: 'first.last@email.com', roles: 'User' },
+  const actions = [
     {
-      name: 'First Last',
-      email: 'first.last@email.com',
-      roles: 'External User',
+      title: "Manage Users",
+      icon: "bi-people-fill",
+      link: "/admin/user-management",
     },
-  ];
-  const cols = [
-    { Header: 'Name', accessor: 'name' },
-    { Header: 'Email', accessor: 'email' },
-    { Header: 'Roles', accessor: 'roles' },
     {
-      Header: 'Actions',
-      id: 'actions',
-      Cell: () => (
-        <div className="d-flex">
-          <Button className="me-2">Edit</Button>
-          <Button variant="danger">Remove</Button>
-        </div>
-      ),
+      title: "Import Data",
+      icon: "bi-layer-backward",
+      link: "/admin/data-import",
     },
-  ];
+  ]
+
   return (
-    <Container fluid="xxl">
-      <Table data={users} columns={cols} options={{ disableFilters: true }} />
+    <Container fluid="xxl" className="my-4">
+      <h1 className="h3 mb-4 text-primary">Administrative Tasks</h1>
+      <hr className="mb-4 mb-4 border-info" />
+
+      <Row>
+        {actions.map((action, index) => (
+          <Col md="3" key={`admin-action-${index}`}>
+            <Link to={action.link}>
+              <Card className="card-link card-accent-primary">
+                <Card.Body className="shadow">
+                  <div className="text-center py-4">
+                    <i className={`display-3 bi ${action.icon}`} role="img" aria-label={action.title} />
+                    <h2 className="h5 text-decoration-none">{action.title}</h2>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
     </Container>
   );
 }
