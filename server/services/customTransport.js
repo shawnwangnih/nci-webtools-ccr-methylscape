44746d260ee31a326c66d9b714ffa19726edc24f
Brian Park <park-brian@users.noreply.github.com>
2022-03-08 18:17:45 -0500

    create custom log transport

---
+++ server/services/customTransport.js
@@ -0,0 +1,25 @@
+const { noop } = require('lodash');
+const Transport = require('winston-transport');
+
+class CustomTransport extends Transport {
+
+    constructor(opts = {}) {
+        super(opts);
+        this.handler = opts.handler || noop;
+    }
+
+    setHandler(handler) {
+        this.handler = handler;
+    }
+
+    log(info, callback) {
+        setImmediate(() => {
+            this.emit('logged', info);
+        });
+
+        this.handler(info);
+        callback();
+    }
+}
+
+module.exports = CustomTransport;
