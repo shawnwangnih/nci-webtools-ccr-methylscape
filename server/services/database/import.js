aa9839da4f78a4501dc4431d8a5b5959ec96d83b
Brian Park <park-brian@users.noreply.github.com>
2022-03-08 18:17:33 -0500

    add database import service

---
+++ server/services/database/import.js
@@ -0,0 +1,33 @@
+const { 
+  createRecordIterator, 
+  importTable, 
+  withDuration,
+  resetTables
+} = require("./utils");
+
+const tables = [
+  "sampleCoordinates", 
+  "sample", 
+  "annotations"
+];
+
+async function importData(connection, logger, sources) {
+  await connection.transaction(async transaction => {
+    logger.info('Initialized data import');
+    for (const table of tables) {
+      await connection.raw('TRUNCATE ?? RESTART IDENTITY CASCADE', [table]);
+    }
+
+    for (const { sourcePath, description, table, columns } of sources) {
+      logger.info(`Importing ${table} table (${description})`);
+      const { results, duration } = await withDuration(async () => {
+        const records = await createRecordIterator(sourcePath, columns);
+        return await importTable(transaction, records, table, logger);
+      });
+      logger.info(`Finished importing ${table} table in ${Math.round(duration)}s (${Math.round(results / duration)} rows/s)`);
+    }
+    return true;
+  });
+}
+
+module.exports = { importData };
