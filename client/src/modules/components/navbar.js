d79c43c36e0882d9bc7b5be6716b01a9d662c321
Brian Park <park-brian@users.noreply.github.com>
2022-03-15 23:24:37 -0400

    enable links when logged in

--- client/src/modules/components/navbar.js
+++ client/src/modules/components/navbar.js
@@ -3,37 +3,47 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import { NavLink } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { sessionState } from '../session/session.state';
 
-export default function Navbar({ links = [], className, children }) {
+export default function Navbar({ linkGroups = [[]], className, children }) {
+  const session = useRecoilValue(sessionState);
+
+  console.log(session);
+
+  const shouldShowLink = link => {
+    console.log(link, session);
+    if (session && link.show) {
+      return link.show(session);
+    }
+    return true;
+  }
+
   return (
     <BootstrapNavbar bg="white" variant="light" className={className}>
       <Container fluid>
         {children}
-        <Nav className="me-auto">
-          {links?.map((link) => (
-            <NavLink
-              to={link.path}
-              key={link.path}
-              className={({ isActive }) =>
-                'nav-link' + (isActive ? ' active' : '')
-              }
-              end={link.exact}
-            >
-              {link.title}
-            </NavLink>
-          ))}
-        </Nav>
-        <Nav className="ms-auto">
-          <NavLink
-            to={'admin'}
-            className={({ isActive }) =>
-              'nav-link' + (isActive ? ' active' : '')
-            }
-          >
-            Admin
-          </NavLink>
-          <Button variant="link">Login</Button>
-        </Nav>
+        {linkGroups.map(links => (
+          <Nav>
+            {links?.filter(shouldShowLink).map((link) => (
+              link.native ? (
+                <a href={link.path} className="nav-link">{link.title}</a>
+              ) : (
+                <NavLink
+                  to={link.path}
+                  key={link.path}
+                  className={({ isActive }) =>
+                    'nav-link' + (isActive ? ' active' : '')
+                  }
+                  end={link.exact}
+                >
+                  {link.title}
+                </NavLink>
+              )
+            ))}
+          </Nav>
+        ))}
+
       </Container>
     </BootstrapNavbar>
   );
