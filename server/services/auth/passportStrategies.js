b846be435cfe84e500ae2d821c3e09ea30cdb5c1
Brian Park <park-brian@users.noreply.github.com>
2022-03-15 23:26:53 -0400

    simplify strategies

--- server/services/auth/passportStrategies.js
+++ server/services/auth/passportStrategies.js
@@ -1,6 +1,30 @@
 const { Issuer, Strategy, generators } = require('openid-client');
 const { codeVerifier, codeChallenge, nonce } = generators;
 
+async function createOAuthStrategy({ baseUrl, clientId, clientSecret, redirectUris, params}) {
+  const { Client } = await Issuer.discover(baseUrl);
+
+  const client = new Client({
+    client_id: clientId,
+    client_secret: clientSecret,
+    redirect_uris: redirectUris,
+    response_types: ['code'],
+  });
+
+  params = {
+    code_challenge: codeChallenge(codeVerifier()),
+    code_challenge_method: 'S256',
+    nonce: nonce(),
+    ...params,
+  };
+
+  return new Strategy({ client, params }, (tokenSet, done) => {
+    console.log('tokenSet', tokenSet);
+    done(null, tokenSet.claims());
+  });
+}
+
+
 async function createPkceStrategy({ baseUrl, clientId, redirectUris, params }) {
   const { Client } = await Issuer.discover(baseUrl);
 
@@ -24,19 +48,7 @@ async function createPkceStrategy({ baseUrl, clientId, redirectUris, params }) {
   });
 }
 
-async function createLoginGovStrategy() {
-  return createPkceStrategy({
-    baseUrl: process.env.LOGIN_GOV_AUTHORIZATION_BASE_URL,
-    clientId: process.env.LOGIN_GOV_CLIENT_ID,
-    redirectUris: [process.env.LOGIN_GOV_REDIRECT_URI],
-    params: {
-      scope: 'openid email',
-      acr_values: process.env.LOGIN_GOV_ACR_VALUES,
-    },
-  });
-}
-
 module.exports = {
+  createOAuthStrategy,
   createPkceStrategy,
-  createLoginGovStrategy,
 };
