cefdad8993273af7f4158a7d0ff46c598043127c
Brian Park <park-brian@users.noreply.github.com>
2022-03-15 23:25:13 -0400

    register Session component

--- client/src/app.js
+++ client/src/app.js
@@ -15,47 +15,58 @@ import QCI from './modules/qciReport/qci';
 import Admin from './modules/admin/admin';
 import DataImport from './modules/admin/data-import/data-import';
 import UserManagement from './modules/admin/user-management/user-management';
+import Session from './modules/session/session';
 import ErrorBoundary from './modules/components/error-boundary';
 
 export default function App() {
+
   const navbarLinks = [
-    { path: '/', title: 'Home', exact: true },
-    { path: 'analysis', title: 'Analysis' },
-    { path: 'data/projects', title: 'Projects' },
-    { path: 'data/experiments', title: 'Experiments' },
-    { path: 'data/samples', title: 'Samples' },
-    { path: 'about', title: 'About' },
+    [
+      { path: '/', title: 'Home', exact: true },
+      { path: 'analysis', title: 'Analysis', show: (session) => session.authenticated  },
+      { path: 'data/projects', title: 'Projects', show: (session) => session.authenticated  },
+      { path: 'data/experiments', title: 'Experiments', show: (session) => session.authenticated  },
+      { path: 'data/samples', title: 'Samples', show: (session) => session.authenticated  },
+      { path: 'about', title: 'About' },
+    ],
+    [
+      { path: 'admin', title: 'Admin', show: (session) => session.authenticated },
+      { path: '/api/login', title: 'Login', native: true, show: (session) => !session.authenticated },
+      { path: '/api/logout', title: 'Logout', native: true, show: (session) => session.authenticated },
+    ]
   ];
 
   return (
     <RecoilRoot>
       <Router>
-        <Navbar links={navbarLinks} className="shadow-sm" />
-        <ErrorBoundary
-          fallback={
-            <Alert variant="danger" className="m-5">
-              An internal error prevented this page from loading. Please contact the
-              website administrator if this problem persists.
-            </Alert>
-          }
-        >
-          <Suspense fallback={<Loader message="Loading Page" />}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="analysis" element={<Analysis />} />
-              <Route path="data" element={<Data />}>
-                <Route path="projects" element={<Projects />} />
-                <Route path="experiments" element={<Experiments />} />
-                <Route path="samples" element={<Samples />} />
-              </Route>
-              <Route path="about/*" element={<About />} />
-              <Route path="qci/*" element={<QCI />} />
-              <Route path="admin" element={<Admin />} />
-              <Route path="admin/user-management" element={<UserManagement />} />
-              <Route path="admin/data-import" element={<DataImport />} />
-            </Routes>
-          </Suspense>
-        </ErrorBoundary>
+        <Session>
+          <Navbar linkGroups={navbarLinks} className="shadow-sm" />
+          <ErrorBoundary
+            fallback={
+              <Alert variant="danger" className="m-5">
+                An internal error prevented this page from loading. Please contact the
+                website administrator if this problem persists.
+              </Alert>
+            }
+          >
+            <Suspense fallback={<Loader message="Loading Page" />}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="analysis" element={<Analysis />} />
+                <Route path="data" element={<Data />}>
+                  <Route path="projects" element={<Projects />} />
+                  <Route path="experiments" element={<Experiments />} />
+                  <Route path="samples" element={<Samples />} />
+                </Route>
+                <Route path="about/*" element={<About />} />
+                <Route path="qci/*" element={<QCI />} />
+                <Route path="admin" element={<Admin />} />
+                <Route path="admin/user-management" element={<UserManagement />} />
+                <Route path="admin/data-import" element={<DataImport />} />
+              </Routes>
+            </Suspense>
+          </ErrorBoundary>
+        </Session>
       </Router>
     </RecoilRoot>
   );
