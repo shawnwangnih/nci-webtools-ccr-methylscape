83805fefa6e78b72ec29cabd8394209d5d69cb51
Brian Park <park-brian@users.noreply.github.com>
2022-03-22 16:47:30 -0400

    update import script for remote configuration

--- database/importDatabase.js
+++ database/importDatabase.js
@@ -1,14 +1,17 @@
 import { fileURLToPath } from 'url';
 import { createRequire } from 'module';
 import minimist from 'minimist';
-import { createSchema } from './schema.js';
+import { schema } from './schema.js';
 import {
   createConnection,
   createRecordIterator,
   importTable,
+  initializeTablesForImport,
   withDuration,
-} from './utils.js';
+} from './services/utils.js';
 import { sources } from './sources.js';
+import { getLogger } from './services/logger.js';
+
 
 // determine if this script was launched from the command line
 const isMainModule = process.argv[1] === fileURLToPath(import.meta.url);
@@ -16,21 +19,23 @@ const require = createRequire(import.meta.url);
 
 if (isMainModule) {
   const args = minimist(process.argv.slice(2));
+  const logger = getLogger("import");
   const connection = createConnection(args);
-  await importDatabase(connection, sources);
+  const config = require('./config.json');
+  await importDatabase(connection, sources, logger, config.aws);
   process.exit(0);
 }
 
-export async function importDatabase(connection, sources = sources) {
+export async function importDatabase(connection, sources, logger, config) {
   return await connection.transaction(async (transaction) => {
-    await createSchema(transaction);
+    await initializeTablesForImport(transaction, schema);
     for (const { sourcePath, description, table, columns } of sources) {
-      console.log(`Importing ${table} table (${description})`);
+      logger.info(`Importing ${table} table (${description})`);
       const { results, duration } = await withDuration(async () => {
-        const records = createRecordIterator(sourcePath, columns);
-        return await importTable(transaction, records, table);
+        const records = await createRecordIterator(sourcePath, columns, config);
+        return await importTable(transaction, records, table, logger);
       });
-      console.log(
+      logger.info(
         `Finished importing ${table} table in ${Math.round(
           duration
         )}s (${Math.round(results / duration)} rows/s)`
