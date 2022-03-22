587a48c76699783d78ccf2b2f5c71d90f072b32f
Brian Park <park-brian@users.noreply.github.com>
2022-03-22 16:49:50 -0400

    create declarative schema

--- database/schema.js
+++ database/schema.js
@@ -1,145 +1,168 @@
-export async function createSchema(database) {
-  /**
-   * Note that this will remove all pre-existing tables.
-   */
-  await database.schema.dropTableIfExists("sampleCoordinates");
-  await database.schema.dropTableIfExists("sample");
-  await database.schema.dropTableIfExists("rolePolicy");
-  await database.schema.dropTableIfExists("userRole");
-  await database.schema.dropTableIfExists("role");
-  await database.schema.dropTableIfExists("user");
-  await database.schema.dropTableIfExists("annotations");
-  await database.schema.dropTableIfExists("genes");
-  await database.schema.dropTableIfExists("importLog");
+export const schema = [
 
   /**
-   * Derived from Sample_sheet_master.xlsx
+   * Source: Sample_sheet_master.xlsx
    * Contains sample metadata
    */
-  await database.schema.createTable("sample", function (table) {
-    table.increments("id");
-    table.string("sample");
-    table.string("idatFilename");
-    table.string("primaryCategory");
-    table.string("primaryStudy");
-    table.string("centerMethylation");
-    table.string("matchedCases");
-    table.string("v11b6");
-    table.string("nihLabels");
-    table.string("nciMetric");
-    table.double("age");
-    table.string("sexCongruency");
-    table.string("sexPrediction");
-    table.string("histology");
-    table.string("locationGeneral");
-    table.string("locationSpecific");
-    table.string("subtypeOrPattern");
-    table.string("details");
-    table.string("h3k27me3");
-    table.text("variants");
-    table.string("fusionsOrTranslocations");
-    table.string("variantsReport");
-    table.string("fusionsOrTranslocationsReport");
-    table.double("overallSurvivalMonths");
-    table.integer("overallSurvivalStatus");
-    table.string('who_2007_grade');
-    table.string('sampling');
-    table.string('samplingTreatment');
-  });
+  {
+    name: "sample",
+    import: true,
+    schema: (table) => {
+      table.increments("id");
+      table.string("sample");
+      table.string("idatFilename");
+      table.string("primaryCategory");
+      table.string("primaryStudy");
+      table.string("centerMethylation");
+      table.string("matchedCases");
+      table.string("v11b6");
+      table.string("nihLabels");
+      table.string("nciMetric");
+      table.double("age");
+      table.string("sexCongruency");
+      table.string("sexPrediction");
+      table.string("histology");
+      table.string("locationGeneral");
+      table.string("locationSpecific");
+      table.string("subtypeOrPattern");
+      table.string("details");
+      table.string("h3k27me3");
+      table.text("variants");
+      table.string("fusionsOrTranslocations");
+      table.string("variantsReport");
+      table.string("fusionsOrTranslocationsReport");
+      table.double("overallSurvivalMonths");
+      table.integer("overallSurvivalStatus");
+      table.string('who_2007_grade');
+      table.string('sampling');
+      table.string('samplingTreatment');
+    }
+  },
 
   /**
-   * Derived from anno_tumors6sets1.RData
+   * Sources: anno_<organSystem>.txt
    * Contains coordinates for various dimension reduction plots (umap, densmap, etc.)
    */
-  await database.schema.createTable("sampleCoordinates", function (table) {
-    table.increments("id");
-    table.integer("sampleId").references("sample.id");
-    table.string("organSystem");
-    table.string("embedding");
-    table.double("x");
-    table.double("y");
-  });
+  {
+    name: "sampleCoordinates",
+    import: true,
+    schema: (table) => {
+      table.increments("id");
+      table.integer("sampleId").references("sample.id");
+      table.string("organSystem");
+      table.string("embedding");
+      table.double("x");
+      table.double("y");
+      table.date("importDate");
+    }
+  },
 
   /**
-   * Create the user table.
-   * Each user must have a unique name
+   * Sources: Bins/BAF.bins_ <sample>.txt 
    */
-  await database.schema.createTable("user", function (table) {
-    table.increments("id");
-    table.string("name").notNullable().unique();
-    table.string("firstName");
-    table.string("lastName");
-    table.string("email");
-    table.boolean("active").defaultTo(false);
-    table.timestamp("createdAt").defaultTo(database.fn.now());
-    table.timestamp("updatedAt").defaultTo(database.fn.now());
-  });
+  {
+    name: "bins",
+    import: true,
+    schema: (table) => {
+      table.increments("id");
+      table.integer("sampleId").references("sample.id");
+      table.string("sample");
+      table.string("chromosome");
+      table.integer("start").unsigned();
+      table.integer("end").unsigned();
+      table.string("feature");
+      table.double("value");
+    }
+  },
+
+  /**
+   * Source: genes.csv
+   * Genome references for copy number
+   */
+  {
+    name: "genes",
+    import: true,
+    schema: (table) => {
+      table.increments('id');
+      table.string('chr').index();
+      table.integer('start').index();
+      table.integer('end').index();
+      table.string('geneID');
+    }
+  },
+
+  /**
+   * Stores import job status logs
+   */
+  {
+    name: "importLog",
+    import: false,
+    schema: (table, connection) => {
+      table.increments("id");
+      table.string("status");
+      table.text("log");
+      table.timestamp("createdAt").defaultTo(connection.fn.now());
+      table.timestamp("updatedAt").defaultTo(connection.fn.now());
+    }
+  },
 
   /**
    * Create the role table.
    * Each role must have a unique name
    * Each role can have a description
-   * Roles can be associated with any number of users through the userRole table
    */
-  await database.schema.createTable("role", function (table) {
-    table.increments();
-    table.string("name").notNullable().unique();
-    table.string("description");
-    table.timestamp("createdAt").defaultTo(database.fn.now());
-    table.timestamp("updatedAt").defaultTo(database.fn.now());
-  });
-
-  /**
-   * Creates the userRole table.
-   * Maps a user to a role (many-to-many relationship)
-   */
-  await database.schema.createTable("userRole", function (table) {
-    table.increments();
-    table.integer("userId").notNullable().references("user.id");
-    table.integer("roleId").notNullable().references("role.id");
-    table.timestamp("createdAt").defaultTo(database.fn.now());
-    table.timestamp("updatedAt").defaultTo(database.fn.now());
-  });
+  {
+    name: "role",
+    import: false,
+    schema: (table, connection) => {
+      table.increments("id");
+      table.string("name").notNullable().unique();
+      table.string("description");
+      table.timestamp("createdAt").defaultTo(connection.fn.now());
+      table.timestamp("updatedAt").defaultTo(connection.fn.now());
+    }
+  },
 
   /**
    * Creates the rolePolicy table.
-   * Maps a role to policies (each role can have many policies)
+   * Defines policies for each role. 
    * Each policy defines an action and a resource
    * Example actions: CreateUser, ReadUser, UpdateUser, DeleteUser
    * Example resources: *, 'user:1', etc
    * Application logic determines how to to enforce policies
    * and how to map resources to entities
    */
-  await database.schema.createTable("rolePolicy", function (table) {
-    table.increments();
-    table.integer("roleId").notNullable().references("role.id");
-    table.string("action");
-    table.string("resource");
-    table.timestamp("createdAt").defaultTo(database.fn.now());
-    table.timestamp("updatedAt").defaultTo(database.fn.now());
-  });
+  {
+    name: "rolePolicy",
+    import: false,
+    schema: (table, connection) => {
+      table.increments("id");
+      table.integer("roleId").notNullable().references("role.id");
+      table.string("action");
+      table.string("resource");
+      table.timestamp("createdAt").defaultTo(connection.fn.now());
+      table.timestamp("updatedAt").defaultTo(connection.fn.now());
+    }
+  },
 
   /**
-   * Creates the genes table.
-   * Source: genes.csv
-   * Genome references for copy number
+   * Create the user table.
+   * Each user has a unique name and optionally, a role
    */
-   await database.schema.createTable('genes', function (table) {
-    table.increments('id');
-    table.string('chr').index('');
-    table.integer('start').index('');
-    table.integer('end').index('');
-    table.string('geneID');
-  });
+  {
+    name: "user",
+    import: false,
+    schema: (table, connection) => {
+      table.increments("id");
+      table.integer("roleId").references("role.id");
+      table.string("name").notNullable().unique();
+      table.string("firstName");
+      table.string("lastName");
+      table.string("email");
+      table.enum("status", ["pending", "inactive", "active"]).defaultTo("pending");
+      table.timestamp("createdAt").defaultTo(connection.fn.now());
+      table.timestamp("updatedAt").defaultTo(connection.fn.now());
+    }
+  },
+];
 
-  /**
-   * Creates the importLog table.
-   */
-   await database.schema.createTable("importLog", function (table) {
-    table.increments("id");
-    table.string("status");
-    table.text("log");
-    table.timestamp("createdAt").defaultTo(database.fn.now());
-    table.timestamp("updatedAt").defaultTo(database.fn.now());
-  });
-}
