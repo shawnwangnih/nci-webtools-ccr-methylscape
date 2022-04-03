7dc0b82d0b5fdca2abae0bd75a26c327f78fd79e
Brian Park <park-brian@users.noreply.github.com>
2022-04-03 09:34:06 -0400

    use s3 provider for queue worker

--- database/startQueueWorker.js
+++ database/startQueueWorker.js
@@ -7,7 +7,10 @@ import { getLogger } from './services/logger.js';
 import { CustomTransport } from './services/transports.js';
 import { loadAwsCredentials } from './services/utils.js';
 import { importDatabase } from './importDatabase.js';
+import { schema } from './schema.js';
 import { sources } from './sources.js';
+import { S3Client } from '@aws-sdk/client-s3';
+import { S3Provider } from './services/providers/s3Provider.js';
 
 // determine if this script was launched from the command line
 const isMainModule = process.argv[1] === fileURLToPath(import.meta.url);
@@ -20,11 +23,13 @@ if (isMainModule) {
 }
 
 export async function startQueueWorker(config) {
-  const sqs = new SQSClient({ region: config.aws.region || 'us-east-1' });
+  const s3 = new S3Client();
+  const sqs = new SQSClient();
   const connection = knex({
     client: 'pg',
     connection: config.database,
   });
+  const sourceProvider = new S3Provider(s3, `s3://${config.aws.s3DataBucket}/${config.aws.s3DataKey}`);
   const logger = getLogger('methylscape-queue-worker');
   const customTransport = new CustomTransport();
   logger.add(customTransport);
@@ -37,25 +42,40 @@ export async function startQueueWorker(config) {
     pollInterval: config.aws.sqsPollInterval || 5,
     messageHandler: async (message) => {
       logger.info('Retrieved message from SQS queue');
-      const { type, importLogId } = message;
-      let logBuffer = '';
-
-      customTransport.setHandler(async (info) => {
-        const logMessage = `${info.timestamp} ${info.message}`;
-        logBuffer += logMessage + '\n';
-
-        await connection('importLog')
-          .where({ id: importLogId })
-          .update({ log: logBuffer, updatedAt: new Date() });
-      });
+      let { type, importLogId } = message;
 
       try {
         if (type === 'importData') {
+          if (!importLogId || isNaN(importLogId)) {
+            const newImportLogRecord = await connection('importLog')
+              .insert({ status: 'PENDING' })
+              .returning('id');
+            importLogId = newImportLogRecord[0].id;
+          }
+
+          customTransport.setHandler(async (info) => {
+            try {
+              const logMessage = `${info.timestamp} ${info.message}`;
+              await connection('importLog')
+                .where({ id: importLogId })
+                .update({ 
+                  log: connection.raw(`concat("log", '\n', ?::text)`, [logMessage]),
+                  updatedAt: new Date() 
+                });
+            } catch(exception) {
+              logger.error(exception.stack);
+              customTransport.setHandler(null);
+              await connection('importLog')
+                .where({ id: importLogId })
+                .update({ status: 'FAILED', updatedAt: new Date() });
+            }
+          });
+          
           await connection('importLog')
             .where({ id: importLogId })
             .update({ status: 'RUNNING', updatedAt: new Date() });
 
-          await importDatabase(connection, sources, logger, config.aws);
+          await importDatabase(connection, schema, sources, sourceProvider, logger);
           logger.info('Finished importing data');
 
           await connection('importLog')
