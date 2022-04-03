539594d474b8f39525f8a655f74a68b78d77f44c
Brian Park <park-brian@users.noreply.github.com>
2022-04-03 09:33:44 -0400

    use providers

--- database/importDatabase.js
+++ database/importDatabase.js
@@ -1,16 +1,20 @@
 import { fileURLToPath } from 'url';
 import { createRequire } from 'module';
-import { schema } from './schema.js';
+import minimist from 'minimist';
 import {
   createConnection,
   createRecordIterator,
   importTable,
-  initializeTablesForImport,
+  importTableFromScript,
+  initializeSchemaForImport,
+  loadAwsCredentials,
+  recreateTable,
   withDuration,
 } from './services/utils.js';
-import { sources } from './sources.js';
+import { LocalProvider } from './services/providers/localProvider.js';
+import { S3Provider } from './services/providers/s3Provider.js';
 import { getLogger } from './services/logger.js';
-
+import { S3Client } from '@aws-sdk/client-s3';
 
 // determine if this script was launched from the command line
 const isMainModule = process.argv[1] === fileURLToPath(import.meta.url);
@@ -18,27 +22,82 @@ const require = createRequire(import.meta.url);
 
 if (isMainModule) {
   const config = require('./config.json');
-  const logger = getLogger("import");
+  const args = minimist(process.argv.slice(2));
+  const schemaPath = args.schema || './schema.js';
+  const sourcesPath = args.sources || './sources.js';
+  const providerName = args.provider || 'local';
+  let providerArgs = [...args._];
+
+  const SourceProvider = {
+    local: LocalProvider,
+    s3: S3Provider
+  }[providerName];
+
+  if (!SourceProvider) {
+    throw new Error(`Unknown provider: ${providerName}`);
+  }
+
+  // if using an s3 provider, 
+  // - load credentials into the environment
+  // - determine the base path for the source data
+  if (SourceProvider === S3Provider) {
+    loadAwsCredentials(config.aws);
+    const s3Client = new S3Client();
+    const basePath = providerArgs[0] || `s3://${config.aws.s3DataBucket}/${config.aws.s3DataKey}`;
+    providerArgs = [s3Client, basePath];
+  }
+
   const connection = createConnection(config.database);
-  await importDatabase(connection, sources, logger, config.aws);
+  const { schema } = await import(schemaPath);
+  const { sources } = await import(sourcesPath);
+  const sourceProvider = new SourceProvider(...providerArgs);
+  const logger = getLogger("import");
+  await importDatabase(connection, schema, sources, sourceProvider, logger);
   process.exit(0);
 }
 
-export async function importDatabase(connection, sources, logger, config) {
-  return await connection.transaction(async (transaction) => {
-    await initializeTablesForImport(transaction, schema);
-    for (const { sourcePath, description, table, columns } of sources) {
-      logger.info(`Importing ${table} table (${description})`);
-      const { results, duration } = await withDuration(async () => {
-        const records = await createRecordIterator(sourcePath, columns, config);
-        return await importTable(transaction, records, table, logger);
-      });
-      logger.info(
-        `Finished importing ${table} table in ${Math.round(
-          duration
-        )}s (${Math.round(results / duration)} rows/s)`
-      );
-    }
+export async function importDatabase(connection, schema, sources, sourceProvider, logger) {
+  // although we would like to use transactions for the
+  // return await connection.transaction(async (transaction) => {
+  return (async (transaction) => {
+    await initializeSchemaForImport(transaction, schema);
+
+    let totalCount = 0;
+    const { results, duration } = await withDuration(async () => {
+      for (let { sourcePath, type, description, table, columns, parseConfig, temporarySchema, importScript } of sources) {
+        const sourcePaths = type === 'folder'
+          ? await sourceProvider.listFiles(sourcePath)
+          : [ sourcePath ];
+  
+        for (const sourcePath of sourcePaths) {
+          logger.info(`Importing ${sourcePath} => ${table} (${description})`);
+  
+          const { results, duration } = await withDuration(async () => {
+            const records = await createRecordIterator(sourcePath, sourceProvider, { columns, parseConfig });
+            const count = (temporarySchema && importScript)
+              ? await importTableFromScript(transaction, records, temporarySchema, importScript, logger)
+              : await importTable(transaction, records, table, logger);
+
+            return count;
+          });
+
+          totalCount += results;
+          logger.info(
+            `Finished importing ${sourcePath} => ${table} in ${(
+              duration.toFixed(2)
+            )}s (${Math.round(results / duration)} rows/s)`
+          );
+        }
+      }
+      return totalCount;
+    });
+
+    logger.info(
+      `Finished importing ${results} rows in ${(
+        duration.toFixed(2)
+      )}s (${Math.round(results / duration)} rows/s)`
+    );
+
     return true;
-  });
+  })(connection);
 }
