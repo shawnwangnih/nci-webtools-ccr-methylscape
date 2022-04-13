2ba52e8707d58396d946c471ae632c7fe5cd297b
Thuong Nguyen <nhoktenz.tech@gmail.com>
2022-04-13 14:19:56 -0400

    refactoring the table.js into 2 different files

---
+++ client/src/modules/analysis/table/groupTables.js
@@ -0,0 +1,63 @@
+import { Button, Tabs, Tab } from 'react-bootstrap';
+import { useRecoilState, useResetRecoilState, useRecoilValue } from 'recoil';
+import { tableForm, tableData } from './table.state';
+import ReactTable from '../../components/table';
+import { selectedPoints } from '../metadata/metadata-plot.state';
+
+export default function GroupTables() {
+  const [formState, setState] = useRecoilState(tableForm);
+  const setForm = (newState) => setState({ ...formState, ...newState });
+  const tables = useRecoilValue(tableData);
+  const umapPoints = useRecoilValue(selectedPoints);
+
+  function handleSelect(e) {
+    setForm({ group: e });
+  }
+
+  console.log(umapPoints);
+  return (
+    <Tabs
+      id="controlled-tab-example"
+      activeKey={formState.group}
+      onSelect={handleSelect}
+      className="mb-3"
+    >
+      {umapPoints.points.map((_, i) => {
+        const { data, cols } = tables[i];
+        return (
+          <Tab
+            key={`table_tab_${i}`}
+            eventKey={`${i}`}
+            title={`Group ${i + 1}`}
+          >
+            {data.length > 0 && (
+              <ReactTable
+                data={data}
+                columns={cols}
+                options={{
+                  initialState: {
+                    hiddenColumns: cols
+                      .filter((col) => col.show === false)
+                      .map((col) => col.accessor),
+                  },
+                }}
+                customOptions={{
+                  hideColumns: true,
+                  download: `Group_${i + 1}_data.csv`,
+                }}
+              />
+            )}
+            {!data.length > 0 && (
+              <div className="d-flex bg-light" style={{ minHeight: '300px' }}>
+                <p className="mx-auto my-auto">
+                  Use Box or Lasso Select in the UMAP plot to view details for
+                  multiple samples.
+                </p>
+              </div>
+            )}
+          </Tab>
+        );
+      })}
+    </Tabs>
+  );
+}
--- client/src/modules/analysis/table/table.js
+++ client/src/modules/analysis/table/table.js
@@ -5,11 +5,15 @@ import Loader from '../../components/loader';
 import ErrorBoundary from '../../components/error-boundary';
 import SurvivalPlot from '../survival/survival-plot';
 import TableTabs from './table-tabs';
+import GroupTabs from './groupTabs';
+import GroupTables from './groupTables';
 
 export default function Table() {
   return (
     <Container fluid>
-      <TableTabs />
+      {/* <TableTabs /> */}
+      <GroupTabs />
+      <GroupTables />
 
       <ErrorBoundary
         fallback={
