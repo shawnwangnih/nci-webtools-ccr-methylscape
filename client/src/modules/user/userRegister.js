7d2261369c57136e0deb2d6a9cad0db2b9ed8d88
Thuong Nguyen <nhoktenz.tech@gmail.com>
2022-04-19 16:49:59 -0400

    Style the register page

--- client/src/modules/user/userRegister.js
+++ client/src/modules/user/userRegister.js
@@ -25,13 +25,13 @@ export default function UserRegister() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const userData = {
-      firstname: state.firstname,
-      lastname: state.lastname,
-      email: state.email,
-      organization: state.organization,
-    };
-    axios.post('', userData).then((response) => {
+    // const userData = {
+    //   firstname: state.firstname,
+    //   lastname: state.lastname,
+    //   email: state.email,
+    //   organization: state.organization,
+    // };
+    axios.post('', state).then((response) => {
       console.log(response.status);
       console.log(response.data);
     });
@@ -39,21 +39,21 @@ export default function UserRegister() {
 
   return (
     <>
-      <Container>
+      <Container fluid="xxl" className="d-flex justify-content-center">
         <Row className="text-white my-3">
           <h1>Register</h1>
         </Row>
       </Container>
       <Container
         fluid="xxl"
-        className="d-inline-flex bg-light justify-content-center mb-2 p-2"
+        className="d-inline-flex justify-content-center mb-2 p-2"
       >
-        <Form className="m-3">
+        <Form className="bg-light p-3">
           <Row>
             <Form.Group className="mb-3" controlId="lastname">
               <Form.Label>Last Name</Form.Label>
               <FormControl
-                type="name"
+                type="text"
                 name="lastname"
                 placeholder="Last Name"
                 value={state.lastname}
@@ -63,7 +63,7 @@ export default function UserRegister() {
             <Form.Group className="mb-3" controlId="firstname">
               <Form.Label>First Name</Form.Label>
               <Form.Control
-                type="name"
+                type="text"
                 name="firstname"
                 placeholder="First Name"
                 value={state.firstname}
@@ -92,9 +92,16 @@ export default function UserRegister() {
               onChange={handleChange}
             />
           </Form.Group>
-          <Button variant="primary" type="submit" onClick={handleSubmit}>
-            Submit
-          </Button>
+          <Row className="d-grid gap-2 col-6 mx-auto">
+            <Button
+              variant="primary"
+              type="submit"
+              className="btn-lg"
+              onClick={handleSubmit}
+            >
+              Submit
+            </Button>
+          </Row>
         </Form>
       </Container>
     </>
