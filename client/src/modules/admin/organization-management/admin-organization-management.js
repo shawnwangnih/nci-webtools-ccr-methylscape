ea5fa82d0176e13087e182c89c930ce4382d7abc
Thuong Nguyen <nhoktenz.tech@gmail.com>
2022-05-02 18:34:14 -0400

    add modal to rename button

--- client/src/modules/admin/organization-management/admin-organization-management.js
+++ client/src/modules/admin/organization-management/admin-organization-management.js
@@ -1,14 +1,24 @@
+import { remove } from 'lodash';
 import React, { useState, useEffect } from 'react';
-import { Container, Button, Row, Modal, Form, Alerta } from 'react-bootstrap';
+import {
+  Container,
+  Button,
+  Row,
+  Modal,
+  Form,
+  FormControl,
+} from 'react-bootstrap';
 import { useRecoilValue, useRecoilRefresher_UNSTABLE } from 'recoil';
 import Table from '../../components/table';
 import { organizationsSelector } from './organization-management.state';
 
 export default function AdminOrganizationManagement() {
   const organizations = useRecoilValue(organizationsSelector);
-  const [form, setForm] = useState({});
-  const [removeForm, setRemoveForm] = useState({});
+  const [form, setForm] = useState([]);
   const [showAddOrgModal, setShowAddOrgModal] = useState(false);
+  const [showRenameOrgModal, setShowRenameOrgModal] = useState(false);
+
+  const [editableRowIndex, setEditableRowIndex] = React.useState(null);
 
   const refreshOrgs = useRecoilRefresher_UNSTABLE(organizationsSelector);
 
@@ -16,16 +26,35 @@ export default function AdminOrganizationManagement() {
     setShowAddOrgModal(true);
   }
 
-  async function handleFormChange(e) {
-    const { name, value } = e.target;
-    setForm((form) => ({ ...form, [name]: value }));
+  async function openRenameOrgModal() {
+    setShowRenameOrgModal(true);
   }
 
-  async function handleFormSubmit(e) {
+  async function addOrganizationChange(e) {
+    const { name, value } = e.target;
+    setForm((form) => ({ ...form, [name]: value }));
+    console.log(form);
+  }
+
+  async function renameOrganizationChange(e) {
+    const { name, value } = e.target;
+    setForm((form) => ({ ...form, [name]: value }));
+    console.log(form);
+  }
+
+  async function handleRemoveOrgChange(cell) {
+    const { id, name } = cell?.row?.original;
+    console.log(id);
+    console.log(name);
+
+    //await axios.delete(`api/organizations/${id}`);
+    //refreshOrgs();
+  }
+
+  function handleSubmit(e) {
     e.preventDefault();
-    setShowAddOrgModal(false);
-    //await axios.put(`api/users/${form.id}`, form);
-    refreshOrgs();
+    console.log('submit');
+    console.log(form);
   }
 
   const cols = [
@@ -45,10 +74,17 @@ export default function AdminOrganizationManagement() {
     {
       Header: 'Actions',
       id: 'actions',
-      Cell: ({ row }) => (
+      Cell: ({ row, setEditableRowIndex, editableRowIndex }) => (
         <div>
-          <Button className="me-2">Rename</Button>
-          <Button className="btn-danger me-2">Remove</Button>
+          <Button className="me-2" onClick={() => openRenameOrgModal()}>
+            Rename
+          </Button>
+          <Button
+            className="btn-danger me-2"
+            onClick={(e) => handleRemoveOrgChange({ row })}
+          >
+            Remove
+          </Button>
         </div>
       ),
     },
@@ -86,15 +122,67 @@ export default function AdminOrganizationManagement() {
           </ul> */}
         </div>
         <Modal show={showAddOrgModal} onHide={() => setShowAddOrgModal(false)}>
-          <Modal.Header closeButton>
-            <Modal.Title>Add New Organization/ Instituiton</Modal.Title>
-          </Modal.Header>
-          <Modal.Body></Modal.Body>
-          <Modal.Footer>
-            <Button variant="primary" type="submit" className="btn-lg">
-              Add
-            </Button>
-          </Modal.Footer>
+          <Form className="bg-light p-3" onSubmit={handleSubmit}>
+            <Modal.Header closeButton>
+              <Modal.Title>Add New Organization/ Instituiton</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+              <Form.Group className="mb-3" controlId="organizationName">
+                <Form.Label>Organization Name</Form.Label>
+                <FormControl
+                  type="text"
+                  name="name"
+                  placeholder="Organization Name"
+                  maxLength={255}
+                  value={form.name}
+                  onChange={addOrganizationChange}
+                  required
+                />
+              </Form.Group>
+            </Modal.Body>
+
+            <Modal.Footer>
+              <Button variant="primary" type="submit" className="btn-lg">
+                Add
+              </Button>
+            </Modal.Footer>
+          </Form>
+        </Modal>
+
+        <Modal
+          show={showRenameOrgModal}
+          onHide={() => setShowRenameOrgModal(false)}
+        >
+          <Form className="bg-light p-3" onSubmit={handleSubmit}>
+            <Modal.Header closeButton>
+              <Modal.Title>Rename Organization</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+              <Form.Group className="mb-3" controlId="organizationName">
+                <Form.Label>Current Organization Name</Form.Label>
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="organizationName">
+                <Form.Label>New Organization Name</Form.Label>
+                <FormControl
+                  type="text"
+                  name="name"
+                  placeholder="Organization Name"
+                  maxLength={255}
+                  value={form.name}
+                  onChange={renameOrganizationChange}
+                  required
+                />
+              </Form.Group>
+            </Modal.Body>
+
+            <Modal.Footer>
+              <Button variant="primary" type="submit" className="btn-lg">
+                Add
+              </Button>
+            </Modal.Footer>
+          </Form>
         </Modal>
       </Container>
     </>
