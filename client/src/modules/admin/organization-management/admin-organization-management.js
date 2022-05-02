6dbed3a0323701c24652cac927e064cbdc7d40c9
Thuong Nguyen <nhoktenz.tech@gmail.com>
2022-05-02 11:38:01 -0400

    add organization list

--- client/src/modules/admin/organization-management/admin-organization-management.js
+++ client/src/modules/admin/organization-management/admin-organization-management.js
@@ -1,7 +1,6 @@
 import { Container, Tab, Tabs } from 'react-bootstrap';
 import { useRecoilValue, useRecoilRefresher_UNSTABLE } from 'recoil';
-import { groupBy } from 'lodash';
-import axios from 'axios';
+import Table from '../../components/table';
 import { organizationsSelector } from './organization-management.state';
 
 export default function AdminOrganizationManagement() {
@@ -11,7 +10,15 @@ export default function AdminOrganizationManagement() {
     <>
       <Container fluid="xxl" className="my-3 text-white rounded h-100">
         <h1 className="mb-1 h2">Admin Organization Management</h1>
-        <div className="bg-white"></div>
+        <div className="bg-white text-primary">
+          <ul>
+            {organizations.map((o) => (
+              <li key={`organization-${o.name}`} value={o.id}>
+                {o.name}
+              </li>
+            ))}
+          </ul>
+        </div>
       </Container>
     </>
   );
