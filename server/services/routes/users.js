fd2c889a089eea72fc408e7bcdaae0b1bded4836
Brian Park <park-brian@users.noreply.github.com>
2022-05-12 17:59:11 -0400

    add utility routes for users

--- server/services/routes/users.js
+++ server/services/routes/users.js
@@ -1,10 +1,21 @@
 const { Router } = require('express');
 const { withAsync } = require('../middleware');
+const { sendNotification } = require('../notifications');
+const config = require('../../config');
 
 const router = Router();
 
-router.post(
+router.get(
     '/users',
+    withAsync(async (request, response) => {
+        const { userManager } = request.app.locals;
+        const results = await userManager.getUsers();
+        response.json(results);
+    })
+);
+
+router.post(
+    '/user',
     withAsync(async (request, response) => {
         const { userManager } = request.app.locals;
         const user = {
@@ -19,16 +30,7 @@ router.post(
 );
 
 router.get(
-    '/users',
-    withAsync(async (request, response) => {
-        const { userManager } = request.app.locals;
-        const results = await userManager.getUsers();
-        response.json(results);
-    })
-);
-
-router.get(
-    '/users/:id',
+    '/user/:id(\\d+)',
     withAsync(async (request, response) => {
         const { userManager } = request.app.locals;
         const { id } = request.params;
@@ -38,18 +40,35 @@ router.get(
 );
 
 router.put(
-    '/users/:id',
+    '/user/:id(\\d+)',
     withAsync(async (request, response) => {
         const { userManager } = request.app.locals;
         const { id } = request.params;
         const user = {...request.body, id, updatedAt: new Date() };
         const results = await userManager.updateUser(user);
+        await sendNotification({
+            userManager,
+            from: config.email.from,
+            to: results.email,
+            subject: 'Methylscape User Account Updated',
+            templateName: 'user-profile-update.html',
+            params: {
+                firstName: results.firstName,
+                lastName: results.lastName,
+                roleName: results.roleName,
+                organizationName: [
+                    results.organizationName,
+                    results.organizationId === 1 && `(${results.organizationOther})`
+                ].filter(Boolean).join(' '),
+                status: results.status
+            }
+        });
         response.json(results);
     })
 )
 
 router.delete(
-    '/users/:id',
+    '/user/:id(\\d+)',
     withAsync(async (request, response) => {
         const { userManager } = request.app.locals;
         const { id } = request.params;
@@ -57,5 +76,107 @@ router.delete(
         response.json(results);
     })
 )
+
+router.post(
+    '/user/register',
+    withAsync(async (request, response) => {
+        const { userManager } = request.app.locals;
+        const user = {
+            ...request.body,
+            name: request.body.email,
+            roleId: null,
+            status: 'pending'
+        }
+        const results = await userManager.addUser(user);
+
+        // send email to user
+        await sendNotification({
+            userManager,
+            from: config.email.from,
+            to: results.email,
+            subject: 'Methylscape User Account - Confirmation',
+            templateName: 'user-registration-confirmation.html',
+            params: {
+                firstName: results.firstName,
+                lastName: results.lastName,
+            }
+        });
+
+        // send emails to admins
+        await sendNotification({
+            userManager,
+            from: config.email.from,
+            roleName: 'admin',
+            subject: 'Methylscape User Account - Review Required',
+            templateName: 'admin-user-registration-review.html',
+            params: {
+                userLastName: results.lastName,
+                userFirstName: results.firstName,
+                userEmail: results.email,
+                organizationName: [
+                    results.organizationName,
+                    results.organizationId === 1 && `(${results.organizationOther})`
+                ].filter(Boolean).join(' '),
+                baseUrl: config.server.baseUrl,
+            }
+        });
+
+        response.json(results);
+    })
+);
+
+router.post(
+    '/user/approve',
+    withAsync(async (request, response) => {
+        const { userManager } = request.app.locals;
+        const user = {
+            id: request.body.id,
+            roleId: request.body.roleId,
+            status: 'active',
+            updatedAt: new Date()
+        }
+        const results = await userManager.updateUser(user);
+        await sendNotification({
+            userManager,
+            from: config.email.from,
+            to: results.email,
+            subject: 'Methylscape Registration Approved',
+            templateName: 'user-registration-approval.html',
+            params: {
+                firstName: results.firstName,
+                lastName: results.lastName,
+                baseUrl: config.server.baseUrl,
+            }
+        });
+        response.json(results);
+    })
+);
+
+router.post(
+    '/user/reject',
+    withAsync(async (request, response) => {
+        const { userManager } = request.app.locals;
+        const user = {
+            id: request.body.id,
+            notes: request.body.notes,
+            status: 'rejected',
+            updatedAt: new Date()
+        }
+        const results = await userManager.updateUser(user);
+        await sendNotification({
+            userManager,
+            from: config.email.from,
+            to: results.email,
+            subject: 'Methylscape Registration Rejected',
+            templateName: 'user-registration-rejection.html',
+            params: {
+                firstName: results.firstName,
+                lastName: results.lastName,
+                notes: results.notes,
+            }
+        });
+        response.json(results);
+    })
+);
 
 module.exports = router;
