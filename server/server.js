eed321bc1c9f3ffc852b428a2208ca02f5ba3366
Brian Park <park-brian@users.noreply.github.com>
2022-05-22 20:24:33 -0400

    remove unused import

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const knex = require('knex');
 const passport = require("passport");
-const nodemailer = require('nodemailer');
 const { registerUserSerializers, registerAuthStrategies } = require("./services/auth/passportUtils");
 const { createSession } = require("./services/session");
 const getLogger = require('./services/logger');
