f1143fb547b04b6e4eb2eb3e1cad0d45818c3ca9
Brian Park <park-brian@users.noreply.github.com>
2022-05-25 21:01:59 -0400

    implement job cancellation

--- database/importDatabase.js
+++ database/importDatabase.js
@@ -21,43 +21,25 @@ const isMainModule = process.argv[1] === fileURLToPath(import.meta.url);
 const require = createRequire(import.meta.url);
 
 if (isMainModule) {
-  const config = require('./config.json');
+  const { aws, database } = require('./config.json');
   const args = minimist(process.argv.slice(2));
   const schemaPath = pathToFileURL(args.schema || './schema.js');
   const sourcesPath = pathToFileURL(args.sources || './sources.js');
   const providerName = args.provider || 'local';
+  const providerArgs = [...args._];
   const forceRecreate = args.recreate;
-  let providerArgs = [...args._];
+  loadAwsCredentials(aws);
 
-  const SourceProvider = {
-    local: LocalProvider,
-    s3: S3Provider
-  }[providerName];
-
-  if (!SourceProvider) {
-    throw new Error(`Unknown provider: ${providerName}`);
-  }
-
-  // if using an s3 provider, 
-  // - load credentials into the environment
-  // - determine the base path for the source data
-  if (SourceProvider === S3Provider) {
-    loadAwsCredentials(config.aws);
-    const s3Client = new S3Client();
-    const basePath = providerArgs[0] || `s3://${config.aws.s3DataBucket}/${config.aws.s3DataKey}`;
-    providerArgs = [s3Client, basePath];
-  }
-
-  const connection = createConnection(config.database);
+  const connection = createConnection(database);
   const { schema } = await import(schemaPath);
   const { sources } = await import(sourcesPath);
-  const sourceProvider = new SourceProvider(...providerArgs);
+  const sourceProvider = getSourceProvider(providerName, providerArgs);
   const logger = getLogger("import");
   await importDatabase(connection, schema, sources, sourceProvider, logger, forceRecreate);
   process.exit(0);
 }
 
-export async function importDatabase(connection, schema, sources, sourceProvider, logger, forceRecreate = false) {
+export async function importDatabase(connection, schema, sources, sourceProvider, logger, forceRecreate = false, shouldCancel = async () => false) {
   const tableSources = sources.filter(source => source.table);
   const postImportSources = sources.filter(source => source.type === 'postImport');
   let totalCount = 0;
@@ -72,12 +54,16 @@ export async function importDatabase(connection, schema, sources, sourceProvider
         const sourcePaths = await getSourcePaths(source, sourceProvider);
   
         for (const sourcePath of sourcePaths) {
+          if (await shouldCancel()) {
+            throw new Error(`Cancelled import`);
+          }
+
           const metadata = getFileMetadataFromPath(sourcePath);
           if (shouldSkip(metadata)) continue;
   
           logger.info(`Importing ${sourcePath} => ${table} (${description})`);
           const { results, duration } = await withDuration(async () => {
-            const records = await createRecordIterator(sourcePath, sourceProvider, { columns, parseConfig });
+            const records = await createRecordIterator(sourcePath, sourceProvider, { columns, parseConfig, logger });
             return await importTable(connection, records, table, logger);
           });
 
@@ -101,14 +87,25 @@ export async function importDatabase(connection, schema, sources, sourceProvider
   logger.info(getStatusMessage({ results, duration }));
 }
 
+function getStatusMessage({ results, duration }) {
+  return `Finished importing ${results} rows in ${(
+    duration.toFixed(2)
+  )}s (${Math.round(results / duration)} rows/s)`;
+}
+
 async function getSourcePaths(source, sourceProvider) {
   return (source.type === 'folder')
     ? await sourceProvider.listFiles(source.sourcePath)
     : [source.sourcePath]
 }
 
-function getStatusMessage({ results, duration }) {
-  return `Finished importing ${results} rows in ${(
-    duration.toFixed(2)
-  )}s (${Math.round(results / duration)} rows/s)`;
+export function getSourceProvider(providerName, providerArgs) {
+  switch(providerName) {
+    case 'local':
+      return new LocalProvider(...providerArgs);
+    case 's3':
+      return new S3Provider(new S3Client(), ...providerArgs);
+    default:
+      throw new Error(`Unknown provider: ${providerName}`);
+  }
 }
