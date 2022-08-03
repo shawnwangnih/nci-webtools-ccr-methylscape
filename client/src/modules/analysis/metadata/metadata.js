ae125a8f236371facfa8b8bf5ee014ac47371ae0
Phyllip Cho <phyllip3@users.noreply.github.com>
2022-08-03 15:45:15 -0400

    memoize metadata component and automatically switch tabs after plot action

--- client/src/modules/analysis/metadata/metadata.js
+++ client/src/modules/analysis/metadata/metadata.js
@@ -1,11 +1,11 @@
-import { Suspense } from "react";
+import { Suspense, memo } from "react";
 import Alert from "react-bootstrap/Alert";
 import Loader from "../../components/loader";
 import ErrorBoundary from "../../components/error-boundary";
 import MetadataForm from "./metadata-form";
 import MetadataPlot from "./metadata-plot";
 
-export default function Metadata({ className, onSelect }) {
+export function Metadata({ className }) {
   return (
     <div className={className}>
       <MetadataForm className="d-block" />
@@ -17,9 +17,11 @@ export default function Metadata({ className, onSelect }) {
           </Alert>
         }>
         <Suspense fallback={<Loader message="Loading Plot" />}>
-          <MetadataPlot onSelect={onSelect} />
+          <MetadataPlot />
         </Suspense>
       </ErrorBoundary>
     </div>
   );
 }
+
+export const MemoizedMetadata = memo(Metadata);
