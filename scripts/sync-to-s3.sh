3bfd542d58a4e96cbfc835a6ae8321d37b43ffd3
Brian Park <park-brian@users.noreply.github.com>
2022-08-10 13:27:46 -0400

    use sync --exact-timestamps --delete

--- scripts/sync-to-s3.sh
+++ scripts/sync-to-s3.sh
@@ -6,11 +6,11 @@ AWS_PROFILE=example-profile-name
 SOURCE=/example/path/to/source
 DESTINATION=s3://example-bucket/example/path/to/destination
 
+aws s3 sync $SOURCE/CNV/bins/ $DESTINATION/CNV/bins/ --exact-timestamps --delete --profile $AWS_PROFILE 
+aws s3 sync $SOURCE/CNV/segments/ $DESTINATION/CNV/segments/ --exact-timestamps --delete --profile $AWS_PROFILE
 aws s3 cp $SOURCE/anno_heme.txt $DESTINATION/anno_heme.txt --profile $AWS_PROFILE
 aws s3 cp $SOURCE/anno_kidney.txt $DESTINATION/anno_kidney.txt --profile $AWS_PROFILE
 aws s3 cp $SOURCE/anno_neuro.txt $DESTINATION/anno_neuro.txt --profile $AWS_PROFILE
 aws s3 cp $SOURCE/anno_pan.txt $DESTINATION/anno_pan.txt --profile $AWS_PROFILE
 aws s3 cp $SOURCE/anno_sarcoma.txt $DESTINATION/anno_sarcoma.txt --profile $AWS_PROFILE
 aws s3 cp $SOURCE/Sample_sheet_master.xlsx $DESTINATION/Sample_sheet_master.xlsx --profile $AWS_PROFILE
-aws s3 cp $SOURCE/CNV/bins/ $DESTINATION/CNV/bins/ --recursive --profile $AWS_PROFILE 
-aws s3 cp $SOURCE/CNV/segments/ $DESTINATION/CNV/segments/ --recursive --profile $AWS_PROFILE
