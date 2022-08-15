a7d809646a31c54c951730b1ef447534eeaf1a80
Brian Park <park-brian@users.noreply.github.com>
2022-08-14 22:23:27 -0400

    improve log message formatting

--- server/services/logger.js
+++ server/services/logger.js
@@ -5,6 +5,22 @@ const { createLogger, format, transports, info } = require("winston");
 const logConfig = require("../config.json").logs;
 require("winston-daily-rotate-file");
 
+function objectToString(obj) {
+  return ["string", "number"].includes(typeof obj)
+    ? obj
+    : util.inspect(obj, { depth: null, compact: true, breakLength: Infinity });
+}
+
+function formatLogMessage({ label, timestamp, level, message }) {
+  return [
+    [label, process.pid, timestamp, level]
+      .filter(Boolean)
+      .map((s) => `[${s}]`)
+      .join(" "),
+    objectToString(message),
+  ].join(" - ");
+}
+
 function getLogger(name, config = logConfig) {
   const { folder, level } = config;
   fs.mkdirSync(folder, { recursive: true });
@@ -14,9 +30,7 @@ function getLogger(name, config = logConfig) {
     format: format.combine(
       format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
       format.label({ label: name }),
-      format.printf(({ label, timestamp, level, message }) =>
-        [[label, process.pid, timestamp, level].map((s) => `[${s}]`).join(" "), util.format(message)].join(" - ")
-      )
+      format.printf(formatLogMessage)
     ),
     transports: [
       new transports.Console(),
